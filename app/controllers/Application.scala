9ddfda3fba743b24243db56eeb297bcba4a81d15
jorilytter <jori.lytter@gmail.com>
2014-02-05 20:56:21 +0200

    service layer working with rest layer

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,42 +8,44 @@ import play.api.mvc.Controller
 import views.html.defaultpages.badRequest
 import play.api.libs.json.JsError
 import play.api.libs.functional.syntax._
+import fi.jori.todo.service.TaskService
+import fi.jori.todo.model.Task
 
 object Application extends Controller {
 
   val jsonContent = "application/json"
-  type taskType = (String, String)
-  var allTasks: List[taskType] = List.empty
-  
-  private def newTask(topic: String) = {
-    println("adding new task "+topic)
-    allTasks = (java.util.UUID.randomUUID().toString(), topic) :: allTasks
-  }
-  
-  private def formatTasks = for(task <- allTasks) yield Json.obj("id" -> task._1, "topic" -> task._2)
-  
+  val service = new TaskService()
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
   def tasks = Action {
-    def todoTasks = Json.obj("keys" -> Json.toJson(List("eka","toka","kolmas")))
-    Ok(todoTasks).as(jsonContent)
+    def tasks = service.tasks.values
+    Ok(Json.arr(tasks)).as(jsonContent)
   }
 
-  implicit val readCreateTask = ((__ \ 'topic).read[String])
+  implicit val readCreateTask = ((__ \ 'topic).read[String] and (__ \ 'explanation).read[String]) tupled
+  implicit val taskJson = Json.writes[Task]
   
   def createTask = Action(parse.json) { request =>
     println(request.body.toString)
-    request.body.validate[String].map { 
-      case topic => {
-        newTask(topic)
-        println(allTasks.length)
-        def result = Json.obj("tasks" -> formatTasks)
-        Ok(result).as(jsonContent)
+    request.body.validate[(String,String)].map { 
+      case (topic,explanation) => {
+        def task = service.create(Task(topic=topic,explanation=explanation))
+        Ok(Json.toJson(task)).as(jsonContent)
       }
     }.recoverTotal {
       e => BadRequest("Error: " + JsError.toFlatJson(e))
     }
+  }
+  
+  def get(uid: String) = Action {
+    def task = service.find(uid)
+    task match { 
+      case None => BadRequest("Error: Task not found")
+      case _ => Ok(Json.toJson(task.get)).as(jsonContent)
+    }
+      
   }
 }
