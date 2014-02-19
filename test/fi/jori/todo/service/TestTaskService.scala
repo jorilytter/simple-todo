6deb13ef9746eb01af2331b253ca48953adfcc6d
jorilytter <jori.lytter@gmail.com>
2014-02-19 21:50:22 +0200

    Tests added for new find functions and test-database collection emptying before running tests

--- test/fi/jori/todo/service/TestTaskService.scala
+++ test/fi/jori/todo/service/TestTaskService.scala
@@ -5,19 +5,23 @@ import org.junit.runner.RunWith
 import org.specs2.execute.AsResult
 import org.specs2.execute.Result
 import org.specs2.matcher.ResultMatchers
+import org.specs2.mutable.BeforeAfter
 import org.specs2.mutable.Specification
+import com.mongodb.casbah.commons.MongoDBObject
 import fi.jori.todo.model.Task
 import play.api.test.WithApplication
 import org.specs2.runner.JUnitRunner
+import org.specs2.mutable.After
 
 abstract class WithTaskService extends WithApplication {
-  
+
   val service: TaskService = new TaskService()
   def createdTask = service.create("topic of task", "some long explanation of task")
   def update(id: String) = service.update((id: String), "New topic", "improved explanation")
   def updatedNotStartedTask(id: String) = service.update(id, "New topic", "improved explanation")
   
   override def around[T: AsResult](t: => T): Result = super.around {
+    val remove = Task.remove(MongoDBObject.empty)
     t
   }
 }
@@ -29,22 +33,18 @@ class TestTaskService extends Specification with ResultMatchers {
     "have uuid" in new WithTaskService {
       val task = createdTask
       task._id must not be None
-      Task.remove(task)
     }
     "have uuid length greater that zero" in new WithTaskService {
       val task = createdTask
       task._id.get.toString().length() must be > 0
-      Task.remove(task)
     }
     "have a creation date" in new WithTaskService {
       val task = createdTask
       task.created must not be None
-      Task.remove(task)
     }
     "have a creation date before current time" in new WithTaskService {
       val task = createdTask
       task.created.get.getTime() must be <= new Date().getTime()
-      Task.remove(task)
     }
   }
 
@@ -53,19 +53,16 @@ class TestTaskService extends Specification with ResultMatchers {
       val created = createdTask
       val task = updatedNotStartedTask(created._id.get.toString())
       task.started must be equalTo(None)
-      Task.remove(task)
     } 
     "have a new topic" in new WithTaskService {
       val created = createdTask
       val task = updatedNotStartedTask(created._id.get.toString())
       task.topic must not be equalTo(created.topic)
-      Task.remove(task)
     } 
     "have a new explanation" in new WithTaskService {
       val created = createdTask
       val task = updatedNotStartedTask(created._id.get.toString())
       task.explanation must not be equalTo(created.explanation)
-      Task.remove(task)
     } 
   }
 
@@ -74,19 +71,16 @@ class TestTaskService extends Specification with ResultMatchers {
       val created = createdTask
       val task = service.start(created._id.get.toString())
       task._id.get must be equalTo(created._id.get)
-      Task.remove(task)
     }
     "have a start date" in new WithTaskService {
       val created = createdTask
       val task = service.start(created._id.get.toString())
       task.started must not be None
-      Task.remove(task)
     }
     "have a start time after creation time" in new WithTaskService {
       val created = createdTask
       val task = service.start(created._id.get.toString())
       task.created.get.getTime() must be >= created.created.get.getTime()
-      Task.remove(task)
     }
   }
   
@@ -96,7 +90,6 @@ class TestTaskService extends Specification with ResultMatchers {
       val started = service.start(created._id.get.toString())
       val task = service.start(created._id.get.toString())
       task.started.get.getTime() must be equalTo(started.started.get.getTime())
-      Task.remove(task)
     }
   }
   
@@ -111,26 +104,22 @@ class TestTaskService extends Specification with ResultMatchers {
       val created = createdTask
       val task = service.finish(created._id.get.toString())
       task.finished must not be None
-      Task.remove(task)
     }
     "have a finish time after creation time" in new WithTaskService {
       val created = createdTask
       val task = service.finish(created._id.get.toString())
       task.finished.get.getTime() must be >= created.created.get.getTime()
-      Task.remove(task)
     }
     "have a finish time after start time" in new WithTaskService {
       val created = createdTask
       val started = service.start(created._id.get.toString())
       val task = service.finish(created._id.get.toString())
       task.finished.get.getTime() must be >= task.started.get.getTime()
-      Task.remove(task)
     }
     "stay finished" in new WithTaskService {
       val created = createdTask
       val task = service.finish(created._id.get.toString())
       service.start(task._id.get.toString()) must throwA[Exception]
-      Task.remove(task)
     }
   }
   
@@ -138,7 +127,6 @@ class TestTaskService extends Specification with ResultMatchers {
     "return a valid task" in new WithTaskService {
       val created = createdTask
       service.find(created._id.get.toString()) must not be None
-      Task.remove(created)
     }
   }
   
@@ -153,13 +141,11 @@ class TestTaskService extends Specification with ResultMatchers {
       val created = createdTask
       val task = service.finish(created._id.get.toString())
       task.started.get must not be None
-      Task.remove(task)
     }
     "have start time before finish time" in new WithTaskService {
       val created = createdTask
       val task = service.finish(created._id.get.toString())
       task.started.get.getTime() must be <= task.finished.get.getTime()
-      Task.remove(task)
     }
   }
   
@@ -167,8 +153,59 @@ class TestTaskService extends Specification with ResultMatchers {
     "mark it removed" in new WithTaskService {
       val created = createdTask
       val task = service.remove(created._id.get.toString())
-      task.deleted must not be None
-      Task.remove(task)
+      task.removed must not be None
+    }
+  }
+  
+  "Finding all removed tasks" should {
+    "find two tasks" in new WithTaskService {
+      val task1 = service.create("topic 1", "exp 1")
+      val task2 = service.create("topic 2", "exp 2")
+      val removed1 = service.remove(task1._id.get.toString())
+      val removed2 = service.remove(task2._id.get.toString())
+      service.findRemoved.size must be equalTo(2)
+    }
+  }
+  
+  "Finding all finished tasks" should {
+    "find two tasks" in new WithTaskService {
+      val task1 = service.create("topic 1", "exp 1")
+      val task2 = service.create("topic 2", "exp 2")
+      val finished1 = service.finish(task1._id.get.toString())
+      val finished2 = service.finish(task2._id.get.toString())
+      service.findFinished.size must be equalTo(2)
+    }
+    "find two tasks and no removed tasks" in new WithTaskService {
+      val task1 = service.create("topic 1", "exp 1")
+      val task2 = service.create("topic 2", "exp 2")
+      val task3 = service.create("topic 3", "exp 3")
+      val finished1 = service.finish(task1._id.get.toString())
+      val finished2 = service.finish(task2._id.get.toString())
+      val removed3 = service.remove(task3._id.get.toString())
+      service.findFinished.size must be equalTo(2)
+    }
+  }
+  
+  "Finding all started tasks" should {
+    "find one started task" in new WithTaskService {
+      val task1 = service.create("topic 1", "exp 1")
+      val task2 = service.create("topic 2", "exp 2")
+      val task3 = service.create("topic 3", "exp 3")
+      val started = service.start(task1._id.get.toString())
+      val finished = service.finish(task2._id.get.toString())
+      val removed = service.remove(task3._id.get.toString())
+      service.findStarted.size must be equalTo(1)
+    }
+  }
+  
+  "Finding all created tasks" should {
+    "find tasks with only created timestamp" in new WithTaskService {
+      val task1 = service.create("topic 1", "exp 1")
+      val task2 = service.create("topic 2", "exp 2")
+      val task3 = service.create("topic 3", "exp 3")
+      val started = service.start(task1._id.get.toString())
+      val finished = service.finish(task2._id.get.toString())
+      service.findCreated.size must be equalTo(1)
     }
   }
 }
