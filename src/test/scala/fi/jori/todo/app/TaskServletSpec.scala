b3eef0285ac343d6c1f6caaaf8c08e149e694a19
jorilytter <jori.lytter@gmail.com>
2014-05-14 20:53:31 +0300

    Added integration tests for tasks in different phases

--- src/test/scala/fi/jori/todo/app/TaskServletSpec.scala
+++ src/test/scala/fi/jori/todo/app/TaskServletSpec.scala
@@ -33,39 +33,97 @@ class TaskServletSpecIntegration extends MutableScalatraSpec {
   "POST "+newTask+" to /task on servlet" should {
     "return status 200 and have id" in {
       post("/task", newTask, headers) {
-        status must be equalTo(200)
+        status must be equalTo 200
         val createdTask = parse(response.body).extract[Task]
         createdTask.id must not be equalTo(None)
       }
     }
   }
-  "GET todo tasks at /task/todo" should {
-    "return status 200 and a list of tasks" in {
+  "GET task at /task/<id>" should {
+    "return a task" in {
       post("/task", newTask, headers) {
-        status must be equalTo (200)
+        status must be equalTo 200
+        val createdTask = parse(response.body).extract[Task]
+        
+        get("/task/"+createdTask.id.get) {
+          status must be equalTo 200
+          val getTask = parse(response.body).extract[Task]
+          getTask must not be equalTo (None)
+          getTask.id must not be equalTo (None)
+        }
+      }
+    }
+  }
+  "GET todo tasks at /task/todo" should {
+    "return status 200 and a list of tasks to todo" in {
+      post("/task", newTask, headers) {
+        status must be equalTo 200
 
         get("/task/todo") {
-          status must be equalTo (200)
+          status must be equalTo 200
           val todoTasks = parse(response.body).extract[List[Task]]
-          todoTasks.size must be greaterThan (0)
+          todoTasks.size must be greaterThan 0
         }
       }
     }
   }
   "GET todo tasks at /task/ongoing" should {
-    "return status 200 and a list of tasks" in {
+    "return status 200 and a list of ongoing tasks" in {
       post("/task", newTask, headers) {
         status must be equalTo (200)
         val task = parse(response.body).extract[Task]
         
         put("/task/"+task.id.get+"/start") {
-          status must be equalTo (200)
+          status must be equalTo 200
+          val startedTask = parse(response.body).extract[Task]
+          startedTask.started must not be equalTo (None)
+
+          get("/task/todo") {
+            status must be equalTo 200
+            val todoTasks = parse(response.body).extract[List[Task]]
+            todoTasks.size must be greaterThan 0
+          }
         }
+      }
+    }
+  }
+  "GET todo tasks at /task/done" should {
+    "return status 200 and a list of done tasks" in {
+      post("/task", newTask, headers) {
+        status must be equalTo (200)
+        val task = parse(response.body).extract[Task]
         
-        get("/task/todo") {
-          status must be equalTo (200)
-          val todoTasks = parse(response.body).extract[List[Task]]
-          todoTasks.size must be greaterThan (0)
+        put("/task/"+task.id.get+"/finish") {
+          status must be equalTo 200
+          val finishedTask = parse(response.body).extract[Task]
+          finishedTask.finished must not be equalTo (None)
+          finishedTask.started must not be equalTo (None)
+          
+          get("/task/done") {
+            status must be equalTo (200)
+            val todoTasks = parse(response.body).extract[List[Task]]
+            todoTasks.size must be greaterThan 0
+          }
+        }
+      }
+    }
+  }
+  "GET todo tasks at /task/removed" should {
+    "return status 200 and a list of removed tasks" in {
+      post("/task", newTask, headers) {
+        status must be equalTo (200)
+        val task = parse(response.body).extract[Task]
+        
+        put("/task/"+task.id.get+"/remove") {
+          status must be equalTo 200
+          val removedTask = parse(response.body).extract[Task]
+          removedTask.deleted must not be equalTo (None)
+          
+          get("/task/removed") {
+            status must be equalTo 200
+            val todoTasks = parse(response.body).extract[List[Task]]
+            todoTasks.size must be greaterThan 0
+          }
         }
       }
     }
