8a76f54a36a7dab75d97004506f4ff3a473581b5
jorilytter <jori.lytter@gmail.com>
2015-03-15 14:26:43 +0200

    Update scalatra and jetty dependencies. Add embedded jetty runner.

--- build.sbt
+++ build.sbt
@@ -2,3 +2,6 @@ port in container.Configuration := 9000
 
 org.scalastyle.sbt.ScalastylePlugin.Settings
 
+assemblyJarName in assembly := "simple-todo.jar"
+
+mainClass in assembly := Some("JettyLauncher")
--- project/build.scala
+++ project/build.scala
@@ -11,7 +11,7 @@ object SimpletodoBuild extends Build {
   val Name = "simple-todo"
   val Version = "0.1"
   val ScalaVersion = "2.11.6"
-  val ScalatraVersion = "2.3.0"
+  val ScalatraVersion = "2.4.0.M2"
 
   lazy val project = Project (
     "simple-todo",
@@ -27,7 +27,7 @@ object SimpletodoBuild extends Build {
         "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
+        "org.eclipse.jetty" % "jetty-webapp" % "9.1.5.v20140505" % "container;compile",
         "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar")),
 	"org.scalatra" %% "scalatra-json" % ScalatraVersion,
 	"org.json4s"   %% "json4s-jackson" % "3.2.10",
